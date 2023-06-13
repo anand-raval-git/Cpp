525b04db73ebcd5809a046c33ee6d69cd2e767ba
anandraval4 <anandraval.mu@gmai.com>
2023-06-13 22:37:58 +0530

    05_printing_dobuly
    05_printing_dobuly

---
+++ Linked_list/05_printing_dobuly.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next =0;
+        this->prev=0;
+    }
+};
+void print(Node* &head)
+{
+    Node * temp = head;
+    while(temp != NULL)
+    {
+        cout<< temp->data <<" ";
+        temp = temp->next;
+
+    }
+}
+int findLength(Node *&head)
+{
+    Node *temp  = head;
+    int length;
+    while(temp != 0)
+    {
+        temp = temp->next;
+        length++;   
+    }
+    return length;
+}
+int main()
+{
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+
+    first->next = second;
+    second->prev = first;
+
+    second->next = third;
+    third->prev = second;
+
+    print(first);
+    return 0;
+}
