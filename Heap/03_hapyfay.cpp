62c8045ca256479f6da4f2aedccd508b4903c573
anandraval4 <anandraval.mu@gmai.com>
2023-07-20 22:49:26 +0530

    03_hapyfay
    03_hapyfay

---
+++ Heap/03_hapyfay.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void hapify(int arr[],int n,int i)
+{
+    int index = i;
+    int left = 2*i;
+    int right = 2*i+1;
+    int largest = index;
+
+    if(left<=n && arr[left]>arr[largest])
+    {
+        largest = left;
+    }
+    if(right<=n && arr[right]>arr[largest])
+    {
+        largest = right;
+    }
+    if(index != largest)
+    {
+        swap(arr[index],arr[largest]);
+        index = largest;
+        hapify(arr,n,index);
+    }
+}
+void buildheap(int arr[],int n)
+{
+    for(int i=n/2;i>0;i--)
+    {
+        hapify(arr,n,i);
+    }
+
+}
+int main()
+{
+    int arr[]={-1,12,56,43,6,78,87,5,44,3,23,32};
+    int n =11;
+    buildheap(arr,n);
+    cout<<"Pritning heap"<<endl;
+    for(int i=0;i<=n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
